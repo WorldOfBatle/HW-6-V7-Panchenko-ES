1e654e2c7b747491caa991caa2932fc329cf6bb5
Evgeny Panchenko <worldbatle@mail.ru>
2025-03-31 21:36:13 +0300

    Создана базовая заготовка проекта с 6 задачами

---
+++ HW-6/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cmath>
+
+void task1()
+{
+}
+
+void task2()
+{
+}
+
+void task3()
+{
+}
+
+void task4()
+{
+}
+
+void task5()
+{
+}
+
+void task6()
+{
+}
+
+int main()
+{
+    task1();
+    task2();
+    task3();
+    task4();
+    task5();
+    task6();
+
+    return 0;
+}
